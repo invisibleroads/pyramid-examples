3ced6a2610c2cef25e76a4d165963a18dd8418b2
Roy Hyunjin Han <support@invisibleroads.com>
2011-04-22 23:01:46 -0400

    Addressing PyPI error

--- board/setup.py
+++ board/setup.py
@@ -34,7 +34,7 @@ setup(
     long_description=README + '\n\n' +  CHANGES,
     classifiers=[
         'Programming Language :: Python',
-        'Framework :: Pyramid',
+        'Framework :: Pylons',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
     ],
